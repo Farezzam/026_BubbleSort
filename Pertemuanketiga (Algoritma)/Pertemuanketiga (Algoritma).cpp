3f133d2fc67f1f68f9197a3b563c9f8c6b4570f8
ASUS <faris13abiyu@gmail.com>
2024-03-07 19:36:39 +0700

    <Prosedur Display>

--- Pertemuanketiga (Algoritma)/Pertemuanketiga (Algoritma).cpp
+++ Pertemuanketiga (Algoritma)/Pertemuanketiga (Algoritma).cpp
@@ -46,7 +46,23 @@ void bubbleSortArray() { // procedur untuk mengurutkan array dengan metode bubbl
 	} while (pass <= n - 1); //step 5
 }
 
-void display() {}
+void display() {
+	cout << endl;
+	cout << "===================================" << endl;
+	cout << "Elemen Array yang telah tersusun" << endl;
+	cout << "===================================" << endl;
+	cout << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j];
+		if (j < n - 1) {
+			cout << " --> ";
+		}
+	}
+	cout << endl;
+	cout << endl;
+	cout << "Jumlah pass = " << n - 1 << endl;
+	cout << endl;
+}
 
 
 
